68033f2ca40ac0137ce701892f9fb8ceb0de35c9
NicoleElazar <nicole.elazar@gmail.com>
2019-10-15 12:01:46 +0200

    name change

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/to-do-app'),
+      dir: require('path').join(__dirname, './coverage/breweries-notes-app'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
